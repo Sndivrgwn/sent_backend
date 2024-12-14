e447288f912c89eed2a3b1c94abfc0d4017f39e7
Sandi Virgiawan <sandivirgiawan64@gmail.com>
2024-12-14 11:23:35 +0700

    update deploy

---
+++ docker/startup.sh
@@ -0,0 +1,15 @@
+
+#!/bin/sh
+
+sed -i "s,LISTEN_PORT,$PORT,g" /etc/nginx/nginx.conf
+
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+
+
+php-fpm -D
+
+# while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
+
+nginx
