591ac0c998b9163725e0914d1d0ec9d3576dfc00
Sandi Virgiawan <sandivirgiawan64@gmail.com>
2025-01-31 19:39:03 +0700

    add update img

--- app/Http/Controllers/Auth/imgController.php
+++ app/Http/Controllers/Auth/imgController.php
@@ -5,28 +5,32 @@ namespace App\Http\Controllers\auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class imgController extends Controller
 {
     public function updateImg(Request $request, $id)
-    {
-        $request->validate([
-            'image' => 'required|url', // Hanya menerima URL
-        ]);
+{
+    Log::info($request->all()); // Debug request
 
-        $item = User::find($id);
+    $request->validate([
+        'image' => 'required|url',
+    ]);
 
-        if (!$item) {
-            return response()->json(['message' => 'Data not found'], 404);
-        }
-
-        $item->image = $request->image;
-        $item->save();
-
-        return response()->json([
-            'message' => 'Image URL updated successfully',
-            'image' => $item->image
-        ], 200);
+    $item = User::find($id);
+    
+    if (!$item) {
+        return response()->json(['message' => 'Data not found'], 404);
     }
+
+    $item->img = $request->image;
+    $item->save();
+
+    return response()->json([
+        'message' => 'Image URL updated successfully',
+        'image' => $item->img
+    ], 200);
+}
+
 
 }
